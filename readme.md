307a275ad306c70f24bba77cfc71eaf54f81ba37
jskad <j.skadborg00@gmail.com>
2023-03-28 16:42:10 +0200

    opencv working on system

---
+++ readme.md
@@ -0,0 +1,4 @@
+Set the **VCPKGRS_DYNAMIC** environment variable to *1*: Open your command prompt and run the following command:
+```bash
+set VCPKGRS_DYNAMIC=1
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate opencv;
+use opencv::imgcodecs::IMREAD_COLOR;
+use opencv::imgcodecs::imread;
+
+use opencv::{
+    core,
+    highgui,
+    prelude::*,
+    Result,
+};
+
+fn run() -> Result<()> {
+    // Load an image
+    let filename = "Sad cat.png";
+    let img = imread(filename, IMREAD_COLOR)?;
+
+    // Display the image
+    highgui::imshow("image", &img)?;
+    highgui::wait_key(0)?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        println!("Error: {}", e);
+    }
 }
